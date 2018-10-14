90ccd1963ae0a816d8c871acb25c07e3949fc5c6
y-akamatsu <ak1985tk@gmail.com>
2018-10-14 22:39:59 +0900

    指摘箇所修正

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
+app.get("/", (req, res) => {
     res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res){
-    let thing = req.params.thing;
+    const thing = req.params.thing;
     res.render("love", {thingVar: thing});
 });
 
 app.get("/posts", function(req, res){
-    let posts = [
+    const posts = [
         {title: "Post 1", author: "Susy"},
         {title: "My favorite ramen noodles", author: "Pork bone soy sauce noodles"},
         {title: "I live in", author: "Osaka"}
     ];
 
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts.ejs", {posts});
 })
 app.listen(3000, function(){
     console.log("Server has started!");
